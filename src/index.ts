8e974ddd1c82c656d1c34ab50eab6e8b18758573
jusiho <pierreblas123@gmail.com>
2023-11-13 23:56:32 -0500

    add new endpont user not refistered yet

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export default {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {
+  register({ strapi }) {
     const { transformArgs, getContentTypeArgs } = strapi
       .plugin("graphql")
       .service("builders").utils;
@@ -49,57 +49,183 @@ export default {
       ],
     }));
 
+    // ----------------------------------------------------------------------------------------
     // extensionService.use(({ nexus }) => ({
     //   types: [
     //     nexus.extendType({
     //       type: "Query",
     //       definition: (t) => {
     //         t.field("studentNotCourse", {
-    //           type: "String",
+    //           type: "PopularityResponse",
     //           args: { id: nexus.stringArg() },
     //           resolve: async (root, args, ctx) => {
-    //             const transformedArgs = transformArgs(args, {
-    //               contentType: strapi.contentTypes["api::course.course"],
-    //               usePagination: false,
-    //             });
-    //             const data = await strapi.services["api::course.course"].find({
-    //               filters: { id: args.id },
-    //             });
+    //             // const entries = await strapi.entityService.findMany(
+    //             //   "api::studentCourse.studentCourse",
+    //             //   {
+    //             //     filters: {
+    //             //       title: "Hello World",
+    //             //       createdAt: { $gt: "2021-11-17T14:28:25.843Z" },
+    //             //     },
+    //             //   }
+    //             // );
+
+    //             // Obtener id de todos los registros de studentCourse
+    //             // const data2 = await strapi.services["api::studentCourse.studentCourse"].find({
+    //             //   filters: { id: 25 },
+    //             // });
+    //             // console.log("data2", data2);
+
+    //             const studenCourses = await strapi.entityService.findMany(
+    //               "api::student-course.student-course",
+    //               {
+    //                 populate: ["user", "course"],
+    //                 filters: { course: { id: { $eq: 25 } } },
+    //               }
+    //             );
+    //             console.log("studenCourses", studenCourses);
+
+    //             // obtener id de los usurrios y guardarlos en un arreglo de todos los registros de studenCourses usando map
+    //             const users = studenCourses.map((user) => user.user.id);
+    //             console.log(users);
+
+    //             console.log("studenCourses", studenCourses);
+
     //             const usuarios = await strapi.db
     //               .query("plugin::users-permissions.user")
 
-    //               .findOne({
-    //                 where: { email: "carloblas123@gmail.com" },
-    //                 populate: {
-    //                   student_courses: true,
+    //               .findPage({
+    //                 where: {
+    //                   id: { $in: users },
     //                 },
+    //                 page: 1,
+    //                 pageSize: 15,
     //               });
 
-    //             console.log("user : ", usuarios);
-    //             console.log("Transform : ", transformedArgs);
-    //             console.log("data : ", data);
+    //             console.log("usuarios", usuarios);
 
-    //             const { find } = strapi
-    //               .plugin("graphql")
-    //               .service("builders")
-    //               .get("content-api")
-    //               .buildQueriesResolvers({
-    //                 contentType: strapi.contentTypes["api::course.course"],
-    //               });
-    //             console.log("find : ", find);
-
-    //             return "XD";
+    //             return usuarios;
     //           },
+    //         });
+    //       },
+    //     }),
+    //     nexus.objectType({
+    //       name: "PopularityResponse", // this is our custom object type
+    //       definition(t) {
+    //         t.field("users", {
+    //           // and if requested, a field to query the product content type
+    //           type: "UsersPermissionsUserEntityResponseCollection",
     //         });
     //       },
     //     }),
     //   ],
     //   resolversConfig: {
-    //     "Query.findBySlug": {
+    //     "Query.studentNotCourse": {
     //       auth: false,
     //     },
     //   },
     // }));
+
+    extensionService.use(({ nexus }) => ({
+      types: [
+        nexus.extendType({
+          type: "Query",
+          definition: (t) => {
+            t.field("studentNotCourse", {
+              type: "PopularityResponse",
+              args: {
+                id: nexus.stringArg(),
+                page: nexus.intArg(),
+                pageSize: nexus.intArg(),
+              },
+              resolve: async (root, args, ctx) => ({
+                course: args,
+              }),
+            });
+          },
+        }),
+        nexus.objectType({
+          name: "PopularityResponse", // this is our custom object type
+          definition(t) {
+            t.field("course", {
+              // and if requested, a field to query the product content type
+              // type: "CourseEntityResponseCollection",
+              type: "UsersPermissionsUserEntityResponseCollection",
+              resolve: async (parent, args) => {
+                const { toEntityResponseCollection } = strapi.service(
+                  "plugin::graphql.format"
+                ).returnTypes;
+
+                const studenCourses = await strapi.entityService.findMany(
+                  "api::student-course.student-course",
+                  {
+                    populate: ["user", "course"],
+                    filters: { course: { id: { $eq: parent.course.id } } },
+                  }
+                );
+
+                // obtener id de los usurrios y guardarlos en un arreglo de todos los registros de studenCourses usando map
+                const users = studenCourses.map((user) => user.user.id);
+
+                const usuarios = await strapi.db
+                  .query("plugin::users-permissions.user")
+
+                  .findPage({
+                    where: {
+                      id: { $notIn: users },
+                    },
+                    page: parent.course.page,
+                    pageSize: parent.course.pageSize,
+                  });
+
+                const start = 0;
+                const limit = parent.course.pageSize;
+                // where we provide the resolver as Strapi does not know about relations of our new PopularityResponse type
+                return toEntityResponseCollection(usuarios.results, {
+                  args: { start, limit },
+                  resourceUID: "plugin::users-permissions.user",
+                });
+              },
+            });
+          },
+        }),
+      ],
+      resolversConfig: {
+        "Query.studentNotCourse": {
+          auth: false,
+        },
+      },
+    }));
+
+    //   extensionService.use(({ nexus }) => ({
+    //     // GraphQL SDL
+    //     typeDefs: `
+    //     type City{
+    //       city: String
+    //    }
+
+    //      type Cosas {
+    //          value: City
+    //      }
+
+    //       type Query {
+    //         studentNotCourse: Cosas
+    //       }
+    //  `,
+    //     resolvers: {
+    //       Query: {
+    //         studentNotCourse: {
+    //           resolve() {
+    //             return { value: { city: "Montpellier" } };
+    //           },
+    //         },
+    //       },
+    //     },
+    //     // resolversConfig: {
+    //     //   "Query.findBySlug": {
+    //     //     auth: false,
+    //     //   },
+    //     // },
+    //   }));
   },
 
   /**
@@ -109,5 +235,5 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {},
 };
