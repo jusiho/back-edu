26882c73121b38be467bb9ca0c10fe305ad18d14
jusiho <pierreblas123@gmail.com>
2023-12-28 03:24:16 -0500

    fix server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,11 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  proxy: true,
+  port: env.int('PORT', 1337),  
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env('URL_NGINX', 'https://sarpit.flyteek.com'),
-  port: env.int('PORT', 1337),  
+  url: env('URL_NGINX', 'http://localhost:1337'),
+  proxy:  env('IS_PROXIED', false),
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
