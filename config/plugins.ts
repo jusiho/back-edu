c55f39b05eeb174255ec23fd3b3ca26a041a5574
jusiho <pierreblas123@gmail.com>
2024-03-22 22:16:47 -0500

    config nodemailer

--- config/plugins.ts
+++ config/plugins.ts
@@ -19,6 +19,7 @@ export default ({ env }) => ({
         name: env("SMTP_NAME"),
         host: env("SMTP_HOST", "mail.flyteek.com"),
         port: env("SMTP_PORT", 465),
+        secure: true,
         auth: {
           user: env("SMTP_USERNAME"),
           pass: env("SMTP_PASSWORD"),
