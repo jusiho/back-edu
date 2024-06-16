fe20d86a306a16f320f1bee00a1de33cdcaf7599
jusiho <pierreblas123@gmail.com>
2024-06-16 16:58:04 -0500

    feat: Update middleware and plugin configurations, add lazy loading for images, and update dependencies

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -5,6 +5,7 @@ export default [
     config: {
       contentSecurityPolicy: {
         directives: {
+          "connect-src": ["'self'", "https:"],
           "script-src": [
             "'self'",
             "'unsafe-inline'",
@@ -19,7 +20,16 @@ export default [
             "strapi.io",
             "s3.amazonaws.com",
             "market-assets.strapi.io",
+            "blob:",
+            process.env.BUNNY_PULL_ZONE,
           ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            process.env.BUNNY_PULL_ZONE,
+          ],
+          upgradeInsecureRequests: null,
         },
       },
     },
--- config/plugins.ts
+++ config/plugins.ts
@@ -125,5 +125,15 @@ export default ({ env }) => ({
         "auth": true // Default: false
       },
     },
-  }
+  },
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-bunnynet",
+      providerOptions: {
+        api_key: process.env.BUNNY_API_KEY,
+        storage_zone: process.env.BUNNY_STORAGE_ZONE,
+        pull_zone: process.env.BUNNY_PULL_ZONE,
+      },
+    },
+  },
 });
